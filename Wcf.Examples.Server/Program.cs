7cc2086a69368f03f58f3af1638f23a6102ae088
Dmitrii <dima-stepan@yandex.ru>
2020-07-14 21:12:30 +0300

    projects

---
+++ Wcf.Examples.Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wcf.Examples.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
